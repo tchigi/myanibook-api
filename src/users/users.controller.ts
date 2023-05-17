6ae33010fe13646b01a506426fccebfdeee29b5f
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2023-05-17 16:58:24 +0300

    last fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,7 +25,7 @@ export class UsersController {
 
     @ApiOperation({summary: 'Get all users'})
     @ApiResponse({status: 200, type: [User]})
-    @Roles('ADMIN')
+    @Roles('USER')
     @UseGuards(RolesGuard)
     @Get()
     getAll(){

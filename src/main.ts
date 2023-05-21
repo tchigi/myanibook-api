307fb515e731b29b948bd08e92e459d2291569f5
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2023-05-21 21:36:32 +0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import {ValidationPipe} from "./pipes/validation.pipe";
 
 async function start() {
     const PORT = process.env.PORT || 5000
-    const app = await  NestFactory.create(AppModule)
+    const app = await  NestFactory.create(AppModule, {cors: true})
 
     const config = new DocumentBuilder()
         .setTitle('myanibook-api')
@@ -17,7 +17,6 @@ async function start() {
         .build()
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('/api/docs', app, document)
-
 
     await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }

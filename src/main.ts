6ab65b14625d0e47999abb9b553d7783da78fdb5
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2023-05-31 21:23:17 +0300

    fix: last fixes

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import * as process from "process";
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import {ValidationPipe} from "./pipes/validation.pipe";
 
+
+
+const PORT = process.env.PORT || 5000
 
 async function start() {
-    const PORT = process.env.PORT || 5000
     const app = await  NestFactory.create(AppModule, {cors: true})
 
     const config = new DocumentBuilder()
@@ -18,7 +19,7 @@ async function start() {
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup('/api/docs', app, document)
 
-    await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+    await app.listen(PORT)
 }
 
 start()

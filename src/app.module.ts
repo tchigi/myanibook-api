fe4e1e6f0113b2706cd27c3beb47b3dca7177287
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2023-06-02 21:13:06 +0300

    try to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,7 +27,7 @@ import {UsersInfoConnectiveModule} from "./users-info/users-info-connective/user
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
+            host: '127.0.0.1',
             port: 5432,
             username: 'postgres',
             password: 'root',

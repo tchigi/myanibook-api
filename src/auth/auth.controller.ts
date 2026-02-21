1e9f3b97d6637e18e0e74d7db39be43e0227b3d5
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2026-02-21 18:39:59 +0300

    docs: дополнил Swagger-документацию во всех контроллерах

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import {Body, Controller, Post} from "@nestjs/common";
-import {ApiTags} from "@nestjs/swagger";
+import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {CreateUserDto} from "../users/dto/create-user.dto";
 import {AuthService} from "./auth.service";
 
@@ -10,11 +10,17 @@ export class AuthController {
     constructor(private authService: AuthService) {
     }
 
+    @ApiOperation({summary: 'Вход в аккаунт'})
+    @ApiResponse({status: 200, description: 'Возвращает JWT-токен'})
+    @ApiResponse({status: 401, description: 'Неверный email или пароль'})
     @Post('/login')
     login(@Body() userDto: CreateUserDto) {
         return this.authService.login(userDto)
     }
 
+    @ApiOperation({summary: 'Регистрация нового пользователя'})
+    @ApiResponse({status: 200, description: 'Возвращает JWT-токен'})
+    @ApiResponse({status: 400, description: 'Пользователь с таким email уже существует'})
     @Post('/registration')
     registration(@Body() userDto: CreateUserDto) {
         return this.authService.registration(userDto)

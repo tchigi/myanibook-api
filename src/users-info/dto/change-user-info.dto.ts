4824b764600d25b16914ab1e05c5cdebf7a2a05d
Ilya Chigrinov <chig-ilya2010@yandex.ru>
2026-02-22 16:11:40 +0300

    fix: уточнил тип value в ChangeUserInfoDto

--- src/users-info/dto/change-user-info.dto.ts
+++ src/users-info/dto/change-user-info.dto.ts
@@ -1,8 +1,9 @@
 import {ApiProperty} from "@nestjs/swagger";
 import {IsNotEmpty} from "class-validator";
+import {AnimeEntry} from "../users-info.model";
 
 export class ChangeUserInfoDto {
     @ApiProperty({example: [{id: 20, addedAt: '2024-01-01'}], description: 'Значение: строка для nickname/avatar, массив объектов {id, addedAt} для anime-list'})
     @IsNotEmpty({message: 'Value should not be empty'})
-    readonly value: string | string[]
+    readonly value: string | AnimeEntry[]
 }
